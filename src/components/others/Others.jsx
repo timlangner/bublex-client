e1b0dfdfe151449bc6c852d596c9a6e170d3e38f
Tim <tim.langner0@gmail.com>
2021-03-13 19:36:15 +0100

    :construction: Add "others" panel

---
+++ src/components/others/Others.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './others.scss';
+
+const Others = () => {
+    return (
+        <div className="others-container">
+
+        </div>
+    );
+};
+
+export default Others;
