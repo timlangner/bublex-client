920e9e2e21fbb73d185931d1c8dcaca99f4d061e
Tim <tim.langner0@gmail.com>
2021-03-13 19:36:39 +0100

    :construction: Add serverlist panel

---
+++ src/components/serverlist/Serverlist.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './serverlist.scss';
+
+const Serverlist = () => {
+    return (
+        <div className="server-container">
+
+        </div>
+    );
+};
+
+export default Serverlist;
