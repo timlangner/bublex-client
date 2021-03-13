e084a86882fe462b84036462110ee49932c2ea64
Tim <tim.langner0@gmail.com>
2021-03-13 19:36:53 +0100

    :sparkles: Add panels to view

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,11 +1,23 @@
 import React from "react";
 import Main from "./main/Main";
 import './App.scss';
+import Account from "./account/Account";
+import Serverlist from "./serverlist/Serverlist";
+import Others from "./others/Others";
+import Featured from "./featured/Featured";
 
 const App = () => {
     return (
         <div className="app">
+            <div className="left-panel">
+                <Account />
+                <Others />
+                <Featured />
+            </div>
             <Main />
+            <div className="right-panel">
+                <Serverlist />
+            </div>
         </div>
   );
 }
