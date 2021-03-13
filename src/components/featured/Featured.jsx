1d8f42f0bd231b8c1996cda1fb67acd558eb12e7
Tim <tim.langner0@gmail.com>
2021-03-13 19:36:26 +0100

    :construction: Add featured panel

---
+++ src/components/featured/Featured.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './featured.scss';
+
+const Featured = () => {
+    return (
+        <div className="featured-container">
+
+        </div>
+    );
+};
+
+export default Featured;
