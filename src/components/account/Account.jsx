b660f1531754007f86872974888cb9b2ace862b9
Tim <tim.langner0@gmail.com>
2021-03-13 19:35:58 +0100

    :construction: Add account panel

---
+++ src/components/account/Account.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import './account.scss';
+
+const Account = () => {
+    return (
+        <div className="account-container">
+
+        </div>
+    );
+};
+
+export default Account;
