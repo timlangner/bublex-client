ab1fa52f16378623d894112c62a67b6e87e66255
Tim <tim.langner0@gmail.com>
2021-03-13 22:53:36 +0100

    :sparkles: Add login button

--- src/components/account/Account.jsx
+++ src/components/account/Account.jsx
@@ -1,10 +1,17 @@
 import React from 'react';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faDiscord } from "@fortawesome/free-brands-svg-icons";
 import './account.scss';
 
 const Account = () => {
     return (
         <div className="account-container">
-
+            <div className="login-wrapper">
+                <button className="login-button">
+                    <FontAwesomeIcon icon={faDiscord} /> Login with Discord
+                </button>
+                <p> Login to save your in-game process, level up and get exclusive rewards!</p>
+            </div>
         </div>
     );
 };
