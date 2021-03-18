31ef4f530e33bae159384eb6a114e88dd25e278a
Tim <tim.langner0@gmail.com>
2021-03-18 21:08:58 +0100

    :construction: Add header

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -7,16 +7,28 @@ import Others from "./others/Others";
 import Featured from "./featured/Featured";
 
 const App = () => {
+    const logoUrl = "https://cdn.discordapp.com/attachments/684020121760432169/820415570549276713/bublex.io_logo_original.png";
+
     return (
         <div className="app">
-            <div className="left-panel">
-                <Account />
-                <Others />
-                <Featured />
-            </div>
-            <Main />
-            <div className="right-panel">
-                <Serverlist />
+            <div className="interface-container">
+                <div className="interface">
+                    <div className="header">
+                        <img src={logoUrl} className="logo" alt="bublex-logo" />
+                        <p><span>76</span> players online</p>
+                    </div>
+                    <div className="panels">
+                        <div className="left-panel">
+                            <Account />
+                            <Others />
+                            <Featured />
+                        </div>
+                        <Main />
+                        <div className="right-panel">
+                            <Serverlist />
+                        </div>
+                    </div>
+                </div>
             </div>
         </div>
   );
