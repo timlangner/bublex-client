d54433795067be6de4368d60e1372ae40efd36cc
Tim <tim.langner0@gmail.com>
2021-03-19 19:04:53 +0100

    :construction: Start serverlist

--- src/components/serverlist/Serverlist.jsx
+++ src/components/serverlist/Serverlist.jsx
@@ -4,7 +4,13 @@ import './serverlist.scss';
 const Serverlist = () => {
     return (
         <div className="server-container">
-
+            <div className="current-server-container">
+                <p>Connected to: Megasplit <span>#1</span></p>
+                <hr />
+            </div>
+            <div className="server-list">
+                
+            </div>
         </div>
     );
 };
