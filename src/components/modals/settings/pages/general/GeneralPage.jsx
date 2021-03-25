630f66e1710d5a3c5fdd8c686094138091e2f911
Tim <tim.langner0@gmail.com>
2021-03-25 22:10:24 +0100

    :recycle: Refactor

--- src/components/modals/settings/pages/general/GeneralPage.jsx
+++ src/components/modals/settings/pages/general/GeneralPage.jsx
@@ -1,5 +1,6 @@
 import React, {useState} from "react";
 import './generalPage.scss';
+import RestoreDefaults from "../../restoreDefaults/RestoreDefaults";
 
 const GeneralPage = () => {
     const [hideHats, setHideHats] = useState(false);
@@ -8,64 +9,67 @@ const GeneralPage = () => {
     const [showSkins, setShowSkins] = useState(true);
 
     return (
-        <div className="checkbox-double">
-            <div
-                className="label-group"
-                onClick={() => setHideHats(!hideHats)}
-            >
-                <input
-                    type="checkbox"
-                    checked={hideHats}
-                />
-                <label
-                    style={!hideHats ? {color: 'darkgray'} : null}
+        <>
+            <div className="checkbox-double">
+                <div
+                    className="label-group"
+                    onClick={() => setHideHats(!hideHats)}
                 >
-                    Hide Hats
-                </label>
-            </div>
-            <div
-                className="label-group"
-                onClick={() => setShowMass(!showMass)}
-            >
-                <input
-                    type="checkbox"
-                    checked={showMass}
-                />
-                <label
-                    style={!showMass ? {color: 'darkgray'} : null}
+                    <input
+                        type="checkbox"
+                        checked={hideHats}
+                    />
+                    <label
+                        style={!hideHats ? {color: 'darkgray'} : null}
+                    >
+                        Hide Hats
+                    </label>
+                </div>
+                <div
+                    className="label-group"
+                    onClick={() => setShowMass(!showMass)}
                 >
-                    Show Mass
-                </label>
-            </div>
-            <div
-                className="label-group"
-                onClick={() => setHideNames(!hideNames)}
-            >
-                <input
-                    type="checkbox"
-                    checked={hideNames}
-                />
-                <label
-                    style={!hideNames ? {color: 'darkgray'} : null}
+                    <input
+                        type="checkbox"
+                        checked={showMass}
+                    />
+                    <label
+                        style={!showMass ? {color: 'darkgray'} : null}
+                    >
+                        Show Mass
+                    </label>
+                </div>
+                <div
+                    className="label-group"
+                    onClick={() => setHideNames(!hideNames)}
                 >
-                    Hide Names
-                </label>
-            </div>
-            <div
-                className="label-group"
-                onClick={() => setShowSkins(!showSkins)}
-            >
-                <input
-                    type="checkbox"
-                    checked={showSkins}
-                />
-                <label
-                    style={!showSkins ? {color: 'darkgray'} : null}
+                    <input
+                        type="checkbox"
+                        checked={hideNames}
+                    />
+                    <label
+                        style={!hideNames ? {color: 'darkgray'} : null}
+                    >
+                        Hide Names
+                    </label>
+                </div>
+                <div
+                    className="label-group"
+                    onClick={() => setShowSkins(!showSkins)}
                 >
-                    Show Skins
-                </label>
+                    <input
+                        type="checkbox"
+                        checked={showSkins}
+                    />
+                    <label
+                        style={!showSkins ? {color: 'darkgray'} : null}
+                    >
+                        Show Skins
+                    </label>
+                </div>
             </div>
-        </div>
+            <RestoreDefaults />
+        </>
     );
 };
 
